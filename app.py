b31ad8e1b70d358d8a5b76b849f77f767c4d7892
ElectroSheepH21 <mario210999@gmail.com>
2022-03-16 17:34:40 +0100

    Added some bootstrap and navigation

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route("/")
+def root():
+    return redirect(url_for("home"))
+
+@app.route("/home")
+def home():
+    return render_template("base_home.html")
+
+@app.route("/child0")
+def child0():
+    return render_template("base_child0.html")
+
+@app.route("/child1")
+def child1():
+    return render_template("base_child1.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
